a55111220f83998c5f35d402caa06cd7d3694164
Alejandro Concepción <alconor23@hotmail.com>
2020-10-29 06:36:42 -0500

    Resumen
    Pruebas iniciales de Gthub

---
+++ __manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "fincas_pma",
+
+    'summary': """
+        Adecuación de fincas para Panamá y ejemplo de programación de modulos en ORM-Odoo
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Máestro de fincas con  parametros de Paamama
+    """,
+
+    'author': "Alconsoft",
+    'website': "http://www.alconsoft.net",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1-20/10/24 - 16:53',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'hr', 'project'],
+
+    # always loaded: Aqui se cargan los formularios de vista.
+    # IMPORTANTE: SE QUITA EL CARACTER "#" PARA QUE SE PUEDA CARGAR ARCHIVO CON LA LISTA DE ACCESO DE SEGURIDAD
+    'data': [
+        ####### ESTO IMPEDIA QUE SE PUDIERA VER EL MENU ########################
+        'security/ir.model.access.csv',
+        ###############################
+        'views/views.xml',
+        'views/templates.xml',
+        ####### CARGA AUTOMATICA AL INSTALAR DE DATOS ESTATICOS ########################
+        'static/xls/fincas_pma.fincas_pma.csv'
+        ###############################
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    # Aplicacion:  si aparace cierto (true) esta modulo sera una aplicacion que aprecera en el listado de aplicaciones de odoo.
+    'application': True,
+}
---
+++ models/models.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class fincas_pma(models.Model):
+    _name = 'fincas_pma.fincas_pma'
+    _description = 'fincas_pma.fincas_pma - FINCAS PANAMA V.20/10/24-13:04'
+    ########## A partir de la versión 13.0, un usuario puede iniciar sesión en varias empresas a la vez.
+    #  Esto permite al usuario acceder a información de varias empresas, pero también crear / editar
+    #  registros en un entorno de varias empresas.################
+    _check_company_auto = True
+    ##########################
+
+    name = fields.Char('Nombre de Finca', required=True)
+    active = fields.Boolean('Activo', default=True)
+    code_finca = fields.Char('Referencia', required=True)
+    description = fields.Text(string='Descripción')
+    employee_in_charge = fields.Many2one('hr.employee', string='Empleado', tracking=True, domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    ########## CUANDO SE HAGAN CAMBIOS EN EL MODELO ES NECESARIO DESINSTALAR LA APPS EN ODOO
+    # BORRAR LA APPS DE LA LISTA DE APLICACIONES
+    # Y REINICIAR EL SERVIDOR DE ODOO #####################
+    #company_id = fields.Many2one('res.company', string='Company', required=True, default=lambda self: self.env.company)
+    company_id = fields.Many2one('res.company', compute='_compute_employee_fincas_pma', store=True, readonly=False,
+        default=lambda self: self.env.company, required=True)
+    ###############################
+    value_area_mts = fields.Integer('Area en mts.')
+    value_area_ha = fields.Float(compute="_value_pc", store=True)
+    
+    @api.depends('value_area_mts')
+    def _value_pc(self):
+        for record in self:
+            record.value_area_ha = float(record.value_area_mts) / 10000
+    
+    @api.depends('employee_in_charge')
+    def _compute_employee_fincas_pma(self):
+        for fincas_pma in self.filtered('employee_in_charge'):
+            fincas_pma.company_id = fincas_pma.employee_in_charge.company_id
+
+    # HERENCIA - AMPLIANDO APLICACIONES EXISTENTES
+    #    AHORA AGREGAREMOS UN CAMPO A UN MODELO EXISTENTE;  EN ESTE CASO SERIA EL MODELO PROYECTO
+    #    EL CAMPO A AGREGAR ES: fincas_pma EN EL NOMBRE DEL MODELO:  project.project
+    #    ESTE SE BUSCA EN EL MENU: AJUSTES; OPCIN: TECNICO; SECCION: SECUENCIA E IDENTIFICADRES
+    
+    class FincasProject(models.Model):
+        _inherit = 'project.project'
+        fincas_pma = fields.Many2one('fincas_pma.fincas_pma', string = 'Finca', tracking=True)
+
+    class provincias(models.Model):
+        _name="fincas_pma.provincias"
+
+        name = fields.Char('Nombre de Provincia', required=True)
+        active = fields.Boolean('Activo', default=True)
+        code_provincia = fields.Char('Código', required=True)
+        description = fields.Text(string='Descripción')
