2aa31214f3ae01ad42bff0a7879ad0ea327b1f5b
Alejandro Concepción <alconor23@hotmail.com>
2020-11-19 07:47:32 -0500

    20201119 - 7:45 AM
    CAMBIOS EN LOS MODELOS DE SUBFINCAS, CARGA DE DATOS UP, FINCAS Y SUBFINCAS

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1-20-11-14 - 18:50',
+    'version': '0.1-20-11-19 - 07:45',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr', 'project'],
@@ -34,6 +34,8 @@
         ####### CARGA AUTOMATICA AL INSTALAR DE DATOS ESTATICOS ########################
         'static/xls/fincas_pma.fincas_pma.csv',
         'static/xls/fincas_pma.labores.csv',
+        'static/xls/fincas_pma.up.csv',
+        'static/xls/fincas_pma.subfincas.csv',
         ###############################
     ],
     # only loaded in demonstration mode
