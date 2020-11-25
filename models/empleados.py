4abd333254014bc0972f41997643b995e13e7dc9
naisor <naisor@users.noreply.github.com>
2020-11-24 21:31:44 -0500

    2020-11-24-9:30
    Se agregó la vista del modelo empleados

---
+++ models/empleados.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+# HERENCIA - AMPLIANDO APLICACIONES EXISTENTES
+#    AHORA AGREGAREMOS UN CAMPO A UN MODELO EXISTENTE;  EN ESTE CASO SERIA EL MODELO PROYECTO
+#    EL CAMPO A AGREGAR ES: fincas_pma EN EL NOMBRE DEL MODELO:  project.project
+#    ESTE SE BUSCA EN EL MENU: AJUSTES; OPCIN: TECNICO; SECCION: SECUENCIA E IDENTIFICADRES
+    
+class FincasEmpleados(models.Model):
+    _inherit = 'hr.employee'
+    
+    nempl = fields.Char('Numero de Empleado', required=True, tracking=True)
+    fincas_pma = fields.Many2one('fincas_pma.fincas_pma', string = 'Finca', tracking=True)
+    salario = fields.Char('Salario por horas', required=True, tracking=True)
