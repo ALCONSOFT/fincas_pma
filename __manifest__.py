4fa5affad56fbfd4758294e4796a1a9891b4e436
alconor1 <soporte1@alconsoft.net>
2020-12-05 19:05:21 -0500

    20201205 - 19:00
    Se agregó datos de Finca, Up y Lote en la Kanban de Proyectos.

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.Rama: dev_project-20-12-03 - 20:45',
+    'version': '0.Rama: dev_project-20-12-05 - 06:45',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr', 'project', 'hr_timesheet', 'sale_management','maintenance'],
